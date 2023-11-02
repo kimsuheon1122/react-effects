e513e75dca8f80e8b5318b44af9f8437f4ae74a3
kimsuheon <kimsuheon1122@gmail.com>
2023-11-03 00:40:57 +0900

    practice for cleanupFunction

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -9,17 +9,18 @@ function App() {
 useEffect(() => {
   console.log("I run only once")
 }, []);
-
+// 한번만 실행
 useEffect(() => {
   console.log("I run when 'keyword' changes.")
 }, [keyword])
-
+// 하나의 아이템 변화만 적용
 useEffect(() => {
   console.log("I run when 'conter' changes.")
 }, [counter])
 useEffect(() => {
   console.log("I run when 'keyword' or 'conter' changes.")
 }, [keyword, counter])
+// 둘 중 하나만 변화해도 적용됨
   return (
     <div>
       <input
